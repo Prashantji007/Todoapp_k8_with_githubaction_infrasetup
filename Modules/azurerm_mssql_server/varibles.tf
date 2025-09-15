13c13aee91c1a17fcbc639b627796b06d08b56f7
Prashantji007 <143020634+Prashantji007@users.noreply.github.com>
2025-09-15 19:31:52 +0530

    Update varibles.tf

--- Modules/azurerm_mssql_server/varibles.tf
+++ Modules/azurerm_mssql_server/varibles.tf
@@ -5,7 +5,12 @@ variable "mssql_server" {
         resource_group_name       = string
         location                  = string
         administrator_login_username = string
-        administrator_login_password = string
         tags                      = map(string)
     }))
+}
+
+variable "mssql_admin_password" {
+  description = "Administrator password for MSSQL servers"
+  type        = string
+  sensitive   = true
 }
