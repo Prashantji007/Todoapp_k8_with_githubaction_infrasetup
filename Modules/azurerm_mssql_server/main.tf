e2caa1a1523a439ee7cc6c40aa437806b265edbe
Prashantji007 <143020634+Prashantji007@users.noreply.github.com>
2025-09-15 12:21:29 +0530

    Update main.tf

--- Modules/azurerm_mssql_server/main.tf
+++ Modules/azurerm_mssql_server/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_mssql_server" "mssql_server_todoapp" {
   resource_group_name          = each.value.resource_group_name
   location                     = each.value.location
   version                      = "12.0"
-  administrator_login_username = each.value.administrator_login_username
+  administrator_login          = each.value.administrator_login_username
   administrator_login_password = each.value.administrator_login_password
   minimum_tls_version          = "1.2"
     tags                         = each.value.tags
