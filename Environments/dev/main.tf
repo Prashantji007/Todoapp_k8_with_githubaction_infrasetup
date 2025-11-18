685eab815d454bd0d28004fdc0b2c702ae2793f6
prashant007 <prashantgi1995@gmail.com>
2025-11-18 23:34:42 +0530

    addedd load balancer module

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -6,34 +6,34 @@ module "rg" {
   rgs    = var.mod_rgs
 }
 
-# module "aks" {
-#   depends_on = [module.rg]
-#   source     = "../../Modules/azurerm_AKS"
-#   aks        = var.mod_aks
-# }
+module "aks" {
+  depends_on = [module.rg]
+  source     = "../../Modules/azurerm_AKS"
+  aks        = var.mod_aks
+}
 
-# module "acr" {
-#   depends_on = [module.rg]
-#   source     = "../../Modules/azurerm_ACR"
-#   acr        = var.mod_acr
-# }
+module "acr" {
+  depends_on = [module.rg]
+  source     = "../../Modules/azurerm_ACR"
+  acr        = var.mod_acr
+}
 
-# module "mssql_server" {
-#   depends_on   = [module.rg]
-#   source       = "../../Modules/azurerm_mssql_server"
-#   mssql_server = var.mod_mssql_server
-# }
+module "mssql_server" {
+  depends_on   = [module.rg]
+  source       = "../../Modules/azurerm_mssql_server"
+  mssql_server = var.mod_mssql_server
+}
 
-# module "mssql_db" {
-#   depends_on = [module.mssql_server, module.rg]
-#   source     = "../../Modules/azurerm_mssql_DB"
-#   mssql_db = {
-#     for k, v in var.mod_mssql_db :
-#     k => merge(v, {
-#       server_id = module.mssql_server.mssql_server_ids["mssql1"]
-#     })
-#   }
-# }
+module "mssql_db" {
+  depends_on = [module.mssql_server, module.rg]
+  source     = "../../Modules/azurerm_mssql_DB"
+  mssql_db = {
+    for k, v in var.mod_mssql_db :
+    k => merge(v, {
+      server_id = module.mssql_server.mssql_server_ids["mssql1"]
+    })
+  }
+}
 
 module "netwokring" {
   depends_on = [module.rg]
