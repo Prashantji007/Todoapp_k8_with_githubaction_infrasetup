1e814e4454e4e93d52a68d533c193ca874ed8418
prashant007 <prashantgi1995@gmail.com>
2025-12-29 17:51:16 +0530

    addes RG in eastus region

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -18,121 +18,121 @@ module "acr" {
   acr        = var.mod_acr
 }
 
-module "mssql_server" {
-  depends_on   = [module.rg]
-  source       = "../../Modules/azurerm_mssql_server"
-  mssql_server = var.mod_mssql_server
-}
+# module "mssql_server" {
+#   depends_on   = [module.rg]
+#   source       = "../../Modules/azurerm_mssql_server"
+#   mssql_server = var.mod_mssql_server
+# }
 
-module "mssql_db" {
-  depends_on = [module.mssql_server, module.rg]
-  source     = "../../Modules/azurerm_mssql_DB"
-  mssql_db = {
-    for k, v in var.mod_mssql_db :
-    k => merge(v, {
-      server_id = module.mssql_server.mssql_server_ids["mssql1"]
-    })
-  }
-}
+# module "mssql_db" {
+#   depends_on = [module.mssql_server, module.rg]
+#   source     = "../../Modules/azurerm_mssql_DB"
+#   mssql_db = {
+#     for k, v in var.mod_mssql_db :
+#     k => merge(v, {
+#       server_id = module.mssql_server.mssql_server_ids["mssql1"]
+#     })
+#   }
+# }
 
-module "netwokring" {
-  depends_on = [module.rg]
-  source     = "../../Modules/azurerm_netwroking"
-  vnet       = var.mod_networking
-}
+# module "netwokring" {
+#   depends_on = [module.rg]
+#   source     = "../../Modules/azurerm_netwroking"
+#   vnet       = var.mod_networking
+# }
 
-module "nsg" {
-  depends_on = [module.rg, module.netwokring]
-  source     = "../../Modules/azurerm_NSG"
-  nsg        = var.mod_nsg
+# module "nsg" {
+#   depends_on = [module.rg, module.netwokring]
+#   source     = "../../Modules/azurerm_NSG"
+#   nsg        = var.mod_nsg
 
-}
+# }
 
-module "asg" {
-  depends_on = [module.rg, module.netwokring]
-  source     = "../../Modules/azurerm_ASG"
-  asg        = var.mod_asg
+# module "asg" {
+#   depends_on = [module.rg, module.netwokring]
+#   source     = "../../Modules/azurerm_ASG"
+#   asg        = var.mod_asg
 
-}
-module "nic" {
-  depends_on = [module.netwokring]
-  source     = "../../Modules/azurerm_NIC"
-  nic        = var.mod_nic
-}
+# }
+# module "nic" {
+#   depends_on = [module.netwokring]
+#   source     = "../../Modules/azurerm_NIC"
+#   nic        = var.mod_nic
+# }
 
-module "vm" {
-  depends_on = [module.rg, module.netwokring, module.nic]
-  source     = "../../Modules/azurerm_VM"
-  vms = {
-    for k, v in var.mod_vm :
-    k => merge(v, {
-      network_interface_ids = [module.nic.network_interface_ids[k]]
-  }) }
-}
-
-
-module "bastion_host" {
-  depends_on   = [module.rg, module.netwokring, module.vm]
-  source       = "../../Modules/azurerm_Bastion_Host"
-  bastion_host = var.mod_bastion_host
-}
+# module "vm" {
+#   depends_on = [module.rg, module.netwokring, module.nic]
+#   source     = "../../Modules/azurerm_VM"
+#   vms = {
+#     for k, v in var.mod_vm :
+#     k => merge(v, {
+#       network_interface_ids = [module.nic.network_interface_ids[k]]
+#   }) }
+# }
 
 
+# module "bastion_host" {
+#   depends_on   = [module.rg, module.netwokring, module.vm]
+#   source       = "../../Modules/azurerm_Bastion_Host"
+#   bastion_host = var.mod_bastion_host
+# }
 
-module "kv" {
-  depends_on = [module.rg, module.aks, module.mod_manged_identity]
-  source     = "../../Modules/azurerm_key_valut"
-  kv         = var.mod_kv
-  db_connection_string = var.db_connection_string
-}
 
-module "stg" {
-  depends_on = [module.rg]
-  source     = "../../Modules/azurerm_storage_account"
-  stg        = var.mod_stg
-}
 
-module "logs" {
-  depends_on = [module.rg, module.monitor ]
-  source = "../../Modules/azurerm_log_analytics"
-  logs = var.mod_logs
-}
+# module "kv" {
+#   depends_on = [module.rg, module.aks, module.mod_manged_identity]
+#   source     = "../../Modules/azurerm_key_valut"
+#   kv         = var.mod_kv
+#   db_connection_string = var.db_connection_string
+# }
 
-module "monitor" {
-  depends_on = [ module.rg ]
-  source = "../../Modules/azrerm_azure_monitor"
-  monitor = var.mod_monitor
-}
+# module "stg" {
+#   depends_on = [module.rg]
+#   source     = "../../Modules/azurerm_storage_account"
+#   stg        = var.mod_stg
+# }
 
-module "mgmt" {
-  depends_on = [ module.rg ]
-  source = "../../Modules/azurerm_mgmt_group"
-  mgmt = var.mod_mgmt
-}
+# module "logs" {
+#   depends_on = [module.rg, module.monitor ]
+#   source = "../../Modules/azurerm_log_analytics"
+#   logs = var.mod_logs
+# }
 
-module "policy" {
-  depends_on = [ module.mgmt ]
-  source = "../../Modules/azurerm_policy_mgmt"
-  policy = var.mod_policy
-}
+# module "monitor" {
+#   depends_on = [ module.rg ]
+#   source = "../../Modules/azrerm_azure_monitor"
+#   monitor = var.mod_monitor
+# }
 
-module "LB" {
-  depends_on = [ module.netwokring, module.firewall ]
-  source = "../../Modules/azurerm_Load_balancer_app_gateway"
-  LB = var.mod_LB
-  waf_policy_id  = module.firewall.waf_policy_id
+# module "mgmt" {
+#   depends_on = [ module.rg ]
+#   source = "../../Modules/azurerm_mgmt_group"
+#   mgmt = var.mod_mgmt
+# }
+
+# module "policy" {
+#   depends_on = [ module.mgmt ]
+#   source = "../../Modules/azurerm_policy_mgmt"
+#   policy = var.mod_policy
+# }
+
+# module "LB" {
+#   depends_on = [ module.netwokring, module.firewall ]
+#   source = "../../Modules/azurerm_Load_balancer_app_gateway"
+#   LB = var.mod_LB
+#   waf_policy_id  = module.firewall.waf_policy_id
  
-}
+# }
 
-module "firewall" {
-  depends_on = [ module.rg ]
-  source = "../../Modules/azurerm_Firewall_Policy_Rules"
-  firepoll = var.mod_firepoll
+# module "firewall" {
+#   depends_on = [ module.rg ]
+#   source = "../../Modules/azurerm_Firewall_Policy_Rules"
+#   firepoll = var.mod_firepoll
 
-}
+# }
 
-module "mod_manged_identity" {
-  depends_on = [ module.rg ]
-  source = "../../Modules/azurerm_managed_identity"
-  managed_identity = var.mod_managed_identity
-}
+# module "mod_manged_identity" {
+#   depends_on = [ module.rg ]
+#   source = "../../Modules/azurerm_managed_identity"
+#   managed_identity = var.mod_managed_identity
+# }
