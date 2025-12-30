c81fcf7a67575bcf7fb958c8c2b15932e8b1e31d
prashant007 <prashantgi1995@gmail.com>
2025-12-30 17:12:00 +0530

    updated backend configuration

--- Environments/dev/provider.tf
+++ Environments/dev/provider.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.41.0"
     }
-    
   }
-   
+
   backend "azurerm" {
     resource_group_name  = "Test"
     storage_account_name = "todoterraformstate"
@@ -14,14 +15,10 @@ terraform {
     key                  = "infra/terraform.tfstate"
     use_oidc             = true
   }
- required_version = ">=1.0"
 }
-
 
 provider "azurerm" {
-  features {
-  
-  }
-  subscription_id = "e8adc8a2-d308-4d54-a68d-45a79457a55f"
-}
+  features {}
 
+  use_oidc = true
+}
